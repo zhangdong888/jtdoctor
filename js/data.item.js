badb3ed6e376cd3dd7be4a5915c2cea569c8c016
张东 <zhangdong@yanhuahealth.com.cn>
2019-02-25 10:46:02 +0800

    jtdoctor

---
+++ js/data.item.js
@@ -0,0 +1,18 @@
+var item_names=[{
+        text: '<span style="display:block;float:left;width:50%">待沟通确认</span>',
+       },
+	   {
+        text: '<span style="display:block;float:left;width:50%">一般提交</span><span style="display:block;float:left;width:50%">10元</span>',
+       },{
+         text: '<span style="display:block;float:left;width:50%">心电图检查</span><span style="display:block;float:left;width:50%">20元</span>',
+       },
+	   {
+		text: '<span style="display:block;float:left;width:50%">血压检查</span><span style="display:block;float:left;width:50%">10元</span>',
+       },
+	   {
+        text: '<span style="display:block;float:left;width:50%">骨密度检查</span><span style="display:block;float:left;width:50%">30元</span>',
+       },
+	   {
+         text: '<span style="display:block;float:left;width:50%">彩超检查</span><span style="display:block;float:left;width:50%">50元</span>',
+       },
+	   ]
---
+++ js/layout.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+mui('body').on('click','a',function(){document.location.href=this.href;});
+$(function(){
+			mui('.mui-scroll-wrapper').scroll({
+					deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+				});
+			})
